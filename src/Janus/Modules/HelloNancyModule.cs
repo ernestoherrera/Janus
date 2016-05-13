c904f9d854c4bb7ece5f2de77d492dec4ba2c2bb
ernestoherrera <ernesto.herrerajr@outlook.com>
2016-05-13 18:09:27 -0400

    Janus configuration

---
+++ src/Janus/Modules/HelloNancyModule.cs
@@ -0,0 +1,17 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Janus.Modules
+{
+    public class HelloNancyModule : NancyModule
+    {
+        public HelloNancyModule()
+        {
+            Get["/"] = parameters => "Hello Nancy";
+        }
+    }
+}
---
+++ src/JanusData/DbConnection.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace JanusData
+{
+    public class DbConnection : IDbConnection
+    {
+        private IDbConnection dbConnection;
+        public string ConnectionString
+        {
+            get { return dbConnection.ConnectionString; }
+
+            set { dbConnection.ConnectionString = value; }
+        }
+
+        public int ConnectionTimeout
+        {
+            get { return dbConnection.ConnectionTimeout; }
+        }
+
+        public string Database
+        {
+            get { return dbConnection.Database; }
+        }
+
+        public ConnectionState State
+        {
+            get { return dbConnection.State; }
+        }
+
+        public IDbTransaction BeginTransaction()
+        {
+            return dbConnection.BeginTransaction();
+        }
+
+        public IDbTransaction BeginTransaction(IsolationLevel il)
+        {
+            return dbConnection.BeginTransaction(il);
+        }
+
+        public void Close()
+        {
+            dbConnection.Close();
+        }
+
+        public void ChangeDatabase(string databaseName)
+        {
+            dbConnection.ChangeDatabase(databaseName);
+        }
+
+        public IDbCommand CreateCommand()
+        {
+            return dbConnection.CreateCommand();
+        }
+
+        public void Open()
+        {
+            dbConnection.Open();
+        }
+
+        public void Dispose()
+        {
+            if (dbConnection.State == ConnectionState.Open)
+                dbConnection.Close();
+
+            dbConnection.Dispose();
+        }
+
+        public DbConnection()
+        {
+            var connectionString = "Server=prdev-sql01.production.tradepmr.com;Database=SwampHead;User ID=sa;Password=mib20!!;";
+
+            dbConnection = new SqlConnection(connectionString);
+
+            dbConnection.Open();
+        }
+    }
+}
